4d6c48e3baed70443a0208090a4291bbbfde059c
mapkkkk <epyc7742@126.com>
2024-06-30 13:42:30 +0800

    multi node upload and download test

--- src/main/java/chain/min/Main.java
+++ src/main/java/chain/min/Main.java
@@ -6,18 +6,23 @@ public class Main {
 
     public static void fileTest() throws IOException {
         String serverIP = "192.168.31.51";
-        int serverPort = 5001;
+        int serverPort0 = 5000;
+        int serverPort1 = 5001;
+        // int serverPort2 = 5002;
 
+        // 创建fileOperation对象
         fileOperation fileOp = new fileOperation();
+        // 上传文件路径
         String uploadFilePath = "/home/ipfs/java_cli/test/upload/uploadTest.txt";
-        // String downloadFileHash = "QmRZtguPjYdrDDZ6V2BrsUKfZoptoVPqbyYxzbZD5Wj2sz";
+        // 下载文件哈希值和目标路径
         String downloadFileHash = "QmV2MRPvrkbbHTP7EkPJfoMeZG7QCey9zE5a1EHYCEZ29Q";
         String downloadDst = "/home/ipfs/java_cli/test/download/downloadRes.txt";
-        // 上传
-        String uploadHashRes = fileOp.uploadFile(serverIP, serverPort, uploadFilePath);
+
+        // 向node0上传
+        String uploadHashRes = fileOp.uploadFile(serverIP, serverPort0, uploadFilePath);
         System.out.println("File CID is: " + uploadHashRes);
-        // 下载
-        fileOp.downloadFile(serverIP, serverPort, downloadFileHash, downloadDst);
+        // 从node1下载
+        fileOp.downloadFile(serverIP, serverPort1, downloadFileHash, downloadDst);
     }
 
     public static void main(String[] args) {
